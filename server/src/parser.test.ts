dbfbbcebd2f6e7bb7c1c888e71ea0ebe59829fb0
syuparn <s.hello.spagetti@gmail.com>
2026-02-25 22:43:07 +0900

    add precedense

--- server/src/parser.test.ts
+++ server/src/parser.test.ts
@@ -1,5 +1,29 @@
-import { Parser } from './parser';
+import { Parser, PRESEDENSE } from './parser';
 import { Tokenizer } from './lexer';
+import { NumberExprAST, VariableExprAST } from './ast';
+
+// helper function to create loc
+function toLoc(
+  uri: string,
+  startLine: number,
+  startCharacter: number,
+  endLine: number,
+  endCharacter: number
+) {
+  return {
+    uri: uri,
+    range: {
+      start: {
+        line: startLine,
+        character: startCharacter,
+      },
+      end: {
+        line: endLine,
+        character: endCharacter,
+      },
+    },
+  };
+}
 
 describe('tokens', () => {
   const uri = 'test://test.toy';
@@ -23,5 +47,55 @@ describe('tokens', () => {
     const parser = new Parser(tokenizer);
     expect(parser.curToken.kind).toBe('EOF');
     expect(parser.peekToken.kind).toBe('EOF');
+  });
+});
+
+describe('peekPrecedence', () => {
+  const uri = 'test://test.toy';
+
+  it('precedence: +', () => {
+    const tokenizer = new Tokenizer(uri, '1 + 2');
+    const parser = new Parser(tokenizer);
+    expect(parser.peekPrecedence()).toBe(PRESEDENSE.ADDITIVE);
+  });
+
+  it('precedence: *', () => {
+    const tokenizer = new Tokenizer(uri, '1 * 2');
+    const parser = new Parser(tokenizer);
+    expect(parser.peekPrecedence()).toBe(PRESEDENSE.MULTIPLICATIVE);
+  });
+
+  it('precedence: =', () => {
+    const tokenizer = new Tokenizer(uri, 'a = 2');
+    const parser = new Parser(tokenizer);
+    expect(parser.peekPrecedence()).toBe(PRESEDENSE.ASSIGNMENT);
+  });
+
+  it('precedence: other', () => {
+    const tokenizer = new Tokenizer(uri, 'return a');
+    const parser = new Parser(tokenizer);
+    expect(parser.peekPrecedence()).toBe(PRESEDENSE.LOWESt);
+  });
+});
+
+describe('Number', () => {
+  const uri = 'test://test.toy';
+
+  it('parse number', () => {
+    const tokenizer = new Tokenizer(uri, '1');
+    const parser = new Parser(tokenizer);
+    expect(parser.parseNumber()).toStrictEqual(new NumberExprAST(toLoc(uri, 0, 0, 0, 1), 1));
+  });
+});
+
+describe('Variable', () => {
+  const uri = 'test://test.toy';
+
+  it('parse variable', () => {
+    const tokenizer = new Tokenizer(uri, 'foo');
+    const parser = new Parser(tokenizer);
+    expect(parser.parseVariable()).toStrictEqual(
+      new VariableExprAST(toLoc(uri, 0, 0, 0, 3), 'foo')
+    );
   });
 });
--- server/src/parser.ts
+++ server/src/parser.ts
@@ -1,10 +1,53 @@
 import { Location } from 'vscode-languageserver';
 import { Token, Tokenizer, TokenKind } from './lexer';
+import { NumberExprAST, VariableExprAST } from './ast';
 
 export interface Diagnostic {
   location: Location;
   message: string;
 }
+
+export const PRESEDENSE = {
+  LOWESt: 0,
+  ASSIGNMENT: 10,
+  COMPARISON: 20,
+  ADDITIVE: 30,
+  MULTIPLICATIVE: 40,
+} as const;
+
+const precedences: Record<TokenKind, number> = {
+  // Punctuation (not used as binary operators)
+  ';': PRESEDENSE.LOWESt,
+  '(': PRESEDENSE.LOWESt,
+  ')': PRESEDENSE.LOWESt,
+  '{': PRESEDENSE.LOWESt,
+  '}': PRESEDENSE.LOWESt,
+  '[': PRESEDENSE.LOWESt,
+  ']': PRESEDENSE.LOWESt,
+  ',': PRESEDENSE.LOWESt,
+  // Assignment (lowest precedence among operators)
+  '=': PRESEDENSE.ASSIGNMENT,
+  // Comparison operators
+  '<': PRESEDENSE.COMPARISON,
+  '>': PRESEDENSE.COMPARISON,
+  // Additive operators
+  '+': PRESEDENSE.ADDITIVE,
+  '-': PRESEDENSE.ADDITIVE,
+  // Multiplicative operators (highest precedence)
+  '*': PRESEDENSE.MULTIPLICATIVE,
+  '/': PRESEDENSE.MULTIPLICATIVE,
+  // Keywords (not used as operators)
+  return: PRESEDENSE.LOWESt,
+  var: PRESEDENSE.LOWESt,
+  def: PRESEDENSE.LOWESt,
+  // Literals and identifiers
+  IDENTIFIER: PRESEDENSE.LOWESt,
+  NUMBER: PRESEDENSE.LOWESt,
+  // Special tokens
+  COMMENT: PRESEDENSE.LOWESt,
+  EOF: PRESEDENSE.LOWESt,
+  UNKNOWN: PRESEDENSE.LOWESt,
+} as const;
 
 export class Parser {
   curToken: Token;
@@ -21,12 +64,13 @@ export class Parser {
     return tokenKinds.includes(this.peekToken.kind);
   }
 
-  assign(tokenKinds: TokenKind[]) {
-    // consume token if kind is matched
-    if (tokenKinds.includes(this.peekToken.kind)) {
-      this.curToken = this.peekToken;
-      this.peekToken = this.tokenizer.tokenize();
-    }
+  nextToken() {
+    this.curToken = this.peekToken;
+    this.peekToken = this.tokenizer.tokenize();
+  }
+
+  peekPrecedence() {
+    return precedences[this.peekToken.kind];
   }
 
   expect(tokenKinds: TokenKind[], errMessage: string) {
@@ -36,6 +80,7 @@ export class Parser {
         location: this.curToken.location,
         message: errMessage,
       });
+      this.curToken = this.dummyToken(this.curToken);
       return;
     }
 
@@ -44,7 +89,25 @@ export class Parser {
     this.peekToken = this.tokenizer.tokenize();
   }
 
-  // TODO: parseNumber
-  // TODO: parseVariable
-  // TODO: parseLiteral
+  dummyToken(prevToken: Token): Token {
+    return {
+      kind: 'UNKNOWN',
+      text: '',
+      location: {
+        uri: prevToken.location.uri,
+        range: {
+          start: prevToken.location.range.end,
+          end: prevToken.location.range.end,
+        },
+      },
+    };
+  }
+
+  parseNumber() {
+    return new NumberExprAST(this.curToken.location, parseFloat(this.curToken.text));
+  }
+
+  parseVariable() {
+    return new VariableExprAST(this.curToken.location, this.curToken.text);
+  }
 }
